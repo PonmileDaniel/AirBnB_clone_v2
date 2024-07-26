07fca5d7143762494a963ac5ba7fee07bed14a63
PonmileDaniel <pomiledaniel@gmail.com>
2024-07-26 14:41:42 +0100

    template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Script that runs an app with flask"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Func that call /"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """Func that calls /hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Func for c"""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """Func for python"""
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Func for number"""
+    return "%d is a number" % n
+
+
+@app.route('/number_template/<int:n>:', strict_slashes=False)
+def number_template(n):
+    """Func for number_template"""
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
