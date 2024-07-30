57b7b50609cf407dd2353549bdc9545f5dd7adca
PonmileDaniel <pomiledaniel@gmail.com>
2024-07-30 09:17:15 +0100

    Update state

--- models/state.py
+++ models/state.py
@@ -8,24 +8,26 @@ from sqlalchemy.orm import relationship
 
 STORAGE_TYPE = os.getenv('HBNB_TYPE_STORAGE', 'fs')
 
-class State(BaseModel):
+
+class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    name = ""
 
-    """Grace updates the state models with given attribute"""
     if STORAGE_TYPE == 'db':
+        name = Column(String(128), nullable=False)
         cities = relationship("City", cascade="all, delete-orphan",
                               backref="state")
     else:
+        name = ""
+
         @property
         def cities(self):
-            """Grace updates Getter attribute that returns the list of City
-                instances with state_id equals to the cuurent State."""
+            """Getter attribute that returns the list of City instances
+               with state_id equals to the current State."""
             from models import storage
+            from models.city import City
             city_list = []
             for city in storage.all(City).values():
                 if city.state_id == self.id:
                     city_list.append(city)
-            return city_list
+            return (city_list)
