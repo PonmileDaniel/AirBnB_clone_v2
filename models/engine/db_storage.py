df42c8a5c6c136b372853c031da6307242a873db
PonmileDaniel <pomiledaniel@gmail.com>
2024-07-30 09:07:24 +0100

    Update filestorage and Db

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -64,3 +64,7 @@ class DBStorage:
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """Calls remove"""
+        self.__session.close()
